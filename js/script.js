d7b3016e0ed18b6c1551b4cac82d12ec772ee4af
Lester <lesthoward@gmail.com>
2021-05-23 21:28:25 -0600

    Enabling dark mode by always

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-// all links on the page opens in new window
+// Including dark mode
+const btnDarkMode = document.querySelector('.darkmode');
+
+// When website is charged
+if (localStorage.getItem('darkmode') == 'true') {
+    document.body.classList.add('light')
+}
+
+btnDarkMode.addEventListener( 'click', () => {
+    document.body.classList.toggle('light')
+    if(document.body.classList.contains('light')) {
+        localStorage.setItem('darkmode','true')
+    } else {
+        localStorage.setItem('darkmode', 'false')
+    }
+})
+
+
+// All links on the page opens in new window
 const links = document.querySelectorAll('a')
 links.forEach(linkOne => {
     linkOne.setAttribute('target', '_blank')
 })
 
-// adding "preventDefault" for buttons to download files
+// Adding "preventDefault" for buttons to download files
 const buttonsToDownload = document.querySelectorAll('.files a');
